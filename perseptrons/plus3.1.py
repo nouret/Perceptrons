f7d114bea8661a81d2837dbe0a2b6d6c95182f87
nouret <rfnourettuz@gmail.com>
2017-06-20 21:41:43 +0300

    some is strange

--- perseptrons/plus3.1.py
+++ perseptrons/plus3.1.py
@@ -4,6 +4,7 @@ import queue
 
 global img
 global tmp
+global Plus, Minus
 
 numpy.random.seed(179)
 
@@ -20,6 +21,126 @@ def oval(canv, X2, Y2, R, N, color = "black"):
 
 def mr():
     return (-1 + numpy.random.random() * 2) / 20
+
+def count(Per1, Per2, Per3, N):
+    tmp = 0
+    while True:
+        t = numpy.random.randint(2)
+        if t:
+            p = numpy.random.randint(len(Plus))
+            x, y = Plus[p]
+        else:
+            p = numpy.random.randint(len(Minus))
+            x, y = Minus[p]
+            t = -1
+        in1 = nonlin(Coeff * incircle(x, y, Per1))
+        in2 = nonlin(Coeff * incircle(x, y, Per2))
+        ans = in1 * Per3[0][0] + in2 * Per3[1][0] + Per3[2][0]
+        if ans * t < 0:
+            Per3[0][0] += in1 * t * 0.01
+            Per3[1][0] += in2 * t * 0.01
+            Per3[2][0] += t * 0.01
+            d1 = nonlin(Coeff * incircle(x, y, Per1), True)
+            d2 = nonlin(Coeff * incircle(x, y, Per2), True)
+            coeff = t * d1 * Per3[0][0] * 0.01
+
+            Per1[0][0] += x ** 2 * coeff
+            Per1[1][0] += x * coeff
+            Per1[0][0] += y ** 2 * coeff
+            Per1[2][0] += y * coeff
+            Per1[3][0] += coeff
+            #Per1 /= numpy.sqrt(Per1[0][0] ** 2 + Per1[1][0] ** 2 + Per1[2][0] ** 2 + Per1[3][0] ** 2)
+            coeff = t * d2 * Per3[1][0] * 0.01
+            Per2[0][0] += x ** 2 * coeff
+            Per2[1][0] += x * coeff
+            Per2[0][0] += y ** 2 * coeff
+            Per2[2][0] += y * coeff
+            Per2[3][0] += coeff
+            #Per2 /= numpy.sqrt(Per2[0][0] ** 2 + Per2[1][0] ** 2 + Per2[2][0] ** 2 + Per2[3][0] ** 2)
+
+        tmp += 1
+        if tmp > N:
+            break
+
+    ErRr = 0
+
+    tmp = 0
+    while True:
+        t = 1
+        if t:
+            p = tmp
+            x, y = Plus[p]
+        else:
+            p = numpy.random.randint(len(Minus))
+            x, y = Minus[p]
+            t = -1
+        in1 = nonlin(Coeff * incircle(x, y, Per1))
+        in2 = nonlin(Coeff * incircle(x, y, Per2))
+        ans = in1 * Per3[0][0] + in2 * Per3[1][0] + Per3[2][0]
+        if ans * t < 0:
+            ErRr += 1
+
+        tmp += 1
+        if tmp >= len(Plus):
+            break
+
+    ErRrP = ErRr
+    ErRr = 0
+
+    tmp = 0
+    while True:
+        t = 0
+        if t:
+            p = tmp
+            x, y = Plus[p]
+        else:
+            p = tmp
+            x, y = Minus[p]
+            t = -1
+        in1 = nonlin(Coeff * incircle(x, y, Per1))
+        in2 = nonlin(Coeff * incircle(x, y, Per2))
+        ans = in1 * Per3[0][0] + in2 * Per3[1][0] + Per3[2][0]
+        if ans * t < 0:
+            ErRr += 1
+
+        tmp += 1
+        if tmp >= len(Minus):
+            break
+
+    return ErRrP, ErRr, Per1, Per2, Per3
+
+def expirement(P, Const = 10000): #p1x, p1y, ..., p6x, p6y
+    x1, y1, x2, y2, x3, y3 = P[:6]
+
+    locPer1 = 2 * numpy.random.random((4, 1)) - 1
+    locPer2 = 2 * numpy.random.random((4, 1)) - 1
+    locPer3 = 2 * numpy.random.random((3, 1)) - 1
+
+    locPer1[0][0] = (x2 - x3)*y1 - x1*(y2 - y3) + x3*y2 - x2*y3
+    locPer1[1][0] = x1**2*(y2 - y3) + y1**2*(y2 - y3) + x2**2*y3 + y2**2*y3 - (x2**2 - x3**2 + y2**2 - y3**2)*y1 - (x3**2 + y3**2)*y2
+    locPer1[2][0] = -x1**2*(x2 - x3) - x2**2*x3 - (x2 - x3)*y1**2 - x3*y2**2 + (x2**2 - x3**2 + y2**2 - y3**2)*x1 + (x3**2 + y3**2)*x2
+    locPer1[3][0] = -(x3*y2 - x2*y3)*x1**2 - (x3*y2 - x2*y3)*y1**2 - (x2**2*y3 + y2**2*y3 - (x3**2 + y3**2)*y2)*x1 + (x2**2*x3 + x3*y2**2 - (x3**2 + y3**2)*x2)*y1
+
+    locPer1 /= numpy.sqrt(locPer1[0][0] ** 2 + locPer1[1][0] ** 2 + locPer1[2][0] ** 2 + locPer1[3][0] ** 2)
+
+    x1, y1, x2, y2, x3, y3 = P[6:]
+
+    #by sage
+    locPer2[0][0] = (x2 - x3)*y1 - x1*(y2 - y3) + x3*y2 - x2*y3
+    locPer2[1][0] = x1**2*(y2 - y3) + y1**2*(y2 - y3) + x2**2*y3 + y2**2*y3 - (x2**2 - x3**2 + y2**2 - y3**2)*y1 - (x3**2 + y3**2)*y2
+    locPer2[2][0] = -x1**2*(x2 - x3) - x2**2*x3 - (x2 - x3)*y1**2 - x3*y2**2 + (x2**2 - x3**2 + y2**2 - y3**2)*x1 + (x3**2 + y3**2)*x2
+    locPer2[3][0] = -(x3*y2 - x2*y3)*x1**2 - (x3*y2 - x2*y3)*y1**2 - (x2**2*y3 + y2**2*y3 - (x3**2 + y3**2)*y2)*x1 + (x2**2*x3 + x3*y2**2 - (x3**2 + y3**2)*x2)*y1
+
+    locPer2 /= numpy.sqrt(locPer2[0][0] ** 2 + locPer2[1][0] ** 2 + locPer2[2][0] ** 2 + locPer2[3][0] ** 2)
+
+    locPer3[0][0] = 1 + mr()
+    locPer3[1][0] = 1 + mr()
+    locPer3[2][0] = -3/2 + mr()
+
+    erp, erm, locPer1, locPer2, locPer3 = count(locPer1, locPer2, locPer3, Const) #big constant
+    #erp, erm = 0, 0
+    return (erp / len(Plus))**2 + (erm / len(Minus))**2, locPer1, locPer2, locPer3
+
 
 Per1 = 2 * numpy.random.random((4, 1)) - 1
 Per2 = 2 * numpy.random.random((4, 1)) - 1
@@ -58,6 +179,57 @@ print(len(Minus))
 if len(Plus) < 6:
     exit(0)
 
+D = max(len(img), len(img[0]))
+for i in range(len(Plus)):
+    Plus[i] = (Plus[i][0] / D, Plus[i][1] / D)
+for i in range(len(Minus)):
+    Minus[i] = (Minus[i][0] / D, Minus[i][1] / D)
+
+Min = 2
+S = set()
+while len(S) < 6:
+    S |= {numpy.random.randint(len(Plus))}
+S = [Plus[_] for _ in S]
+MinS = S
+for i in range(50): #big constant
+    S = set()
+    while len(S) < 6:
+        S |= {numpy.random.randint(len(Plus))}
+    S = [Plus[_] for _ in S]
+    P = [S[0][0], S[0][1], S[1][0], S[1][1], S[4][0], S[4][1]]
+    P += [S[3][0], S[3][1], S[2][0], S[2][1], S[5][0], S[5][1]]
+
+    tmpMin, locPer1, locPer2, locPer3 = expirement(P)
+    if tmpMin < Min:
+        MinS = S
+        Min = tmpMin
+
+print("Min = ", Min)
+S = MinS
+P = [S[0][0], S[0][1], S[1][0], S[1][1], S[4][0], S[4][1]]
+P += [S[3][0], S[3][1], S[2][0], S[2][1], S[5][0], S[5][1]]
+
+tmpMin, Per1, Per2, Per3 = expirement(P, Const = 2000000)
+
+print(tmpMin)
+
+print("Start draw")
+
+for i in range(len(img)):
+    for j in range(len(img[0])):
+        x, y = i / D, j / D
+        in1 = nonlin(Coeff * incircle(x, y, Per1))
+        in2 = nonlin(Coeff * incircle(x, y, Per2))
+        ans = in1 * Per3[0][0] + in2 * Per3[1][0] + Per3[2][0]
+        if ans > 0:
+            img[i][j][0] = (240 + img[i][j][0]) // 2
+            img[i][j][1] = (240 + img[i][j][1]) // 2
+            img[i][j][2] = (60 + img[i][j][2]) // 2
+
+cv2.imwrite("./newpm3.jpg", img)
+
+exit(0)
+
 S = set()
 while len(S) < 6:
     S |= {numpy.random.randint(len(Plus))}
@@ -69,12 +241,6 @@ for elem in S:
             img[x + dx][y + dy][0] = 0
             img[x + dx][y + dy][1] = 0
             img[x + dx][y + dy][2] = 255
-
-D = max(len(img), len(img[0]))
-for i in range(len(Plus)):
-    Plus[i] = (Plus[i][0] / D, Plus[i][1] / D)
-for i in range(len(Minus)):
-    Minus[i] = (Minus[i][0] / D, Minus[i][1] / D)
 
 S = [Plus[i] for i in S]
 
@@ -139,8 +305,8 @@ while True:
         #Per2 /= numpy.sqrt(Per2[0][0] ** 2 + Per2[1][0] ** 2 + Per2[2][0] ** 2 + Per2[3][0] ** 2)
 
     tmp += 1
-    print(tmp / (10000 * (len(Plus) + len(Minus))))
-    if tmp > 10000 * (len(Plus) + len(Minus)):
+    print(tmp / (5000 * (len(Plus) + len(Minus))))
+    if tmp > 5000 * (len(Plus) + len(Minus)):
         break
 
 ErRr = 0
@@ -198,8 +364,9 @@ print(Per1)
 print(Per2)
 print(Per3)
 
-C, X2, Y2, R1 = Per1[0][0], Per1[1][0], Per1[2][0], Per1[3][0]
 """
+C, X2, Y2, R1 = Per1[0][0], Per1[1][0], Per1[2][0], Per1[3][0]
+
 if (D * X2 / C / 2)**2 + (D * Y2 / C / 2)**2 - D * D * R1 / C > 0:
     X = -1 * D * X2 / 2 / C
     Y = -1 * D * Y2 / 2 / C
